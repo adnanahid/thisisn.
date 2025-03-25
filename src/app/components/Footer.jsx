81268121e287300aa37bc4b48b172384d9e3c684
adnanahid <sikuahamed@gmail.com>
2025-03-25 17:35:56 +0600

    added footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="tracking-widest font-light px-4 md:px-12 lg:px-24 mt-10 m-4 relative">
-      <p className="text-center text-[10px] md:text-sm">
+      <p className="text-center text-[10px] md:text-xs">
         Copyright © {new Date().getFullYear()} - All rights reserved by Nahid
         Hasan
       </p>
